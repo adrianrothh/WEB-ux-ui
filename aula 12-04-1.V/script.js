d662dbcfac760060ba1149f5a52665b4633f6c12
adrianrothh <167586506+adrianrothh@users.noreply.github.com>
2024-04-19 21:57:51 -0300

    Add files via upload

---
+++ aula 12-04-1.V/script.js
@@ -0,0 +1,16 @@
+alert("Boa noite")
+
+a = 10
+b = 5
+
+function imprimirPagina(){
+    document.write('Ferramentas WEB');
+}
+
+function tamanhoFonte(){
+    document.getElementById('texto').style.fontSize='30px';
+}
+
+function adicionaConteudo(){
+    document.getElementById('inserir').innerHTML = Date();
+}
---
+++ aula 19-04/script.js
@@ -0,0 +1,7 @@
+function number1(){
+    document.getElementById('texto').style.fontSize='30px';
+}
+
+function number2(){
+    document.getElementById('inserir').innerHTML = Date();
+}
